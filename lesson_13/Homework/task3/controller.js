f46337f6fe665b47ff4b1704bf6fb40b3fa28e03
Sergii Ivanchenko <ivanchenko.s@gmail.com>
2017-06-09 13:03:26 +0300

    Lesson13. Homework task 3

---
+++ lesson_13/Homework/task3/controller.js
@@ -0,0 +1,41 @@
+// Условия задачи
+// Создайте форму для регистрации нового пользователя. Форма должна содержать поля:
+//     ФИО, email, телефон, пароль, подтверждение пароля.
+//     Реализуйте real-time валидацию пользовательского ввода.
+
+MyModule.controller ('MainCtrl', MainCtrl);
+
+MainCtrl.$inject = ['$scope'];
+function MainCtrl($scope) {
+
+    $scope.nameRegEx = /(.*[a-z]){3,}/i;
+    $scope.phoneRegEx = /[0-9]{4,}/;
+    $scope.passwordRegEx = /[a-zA-Z0-9]{6,}/;
+
+    $scope.newUser = {
+        userName: "",
+        userEmail: "",
+        userPhone: "",
+        userPass1: "",
+        userPass2: ""
+    };
+
+    $scope.isValideClass = "";
+    $scope.submitBtnClass = "btn-default";
+    $scope.message = "";
+
+    $scope.isFormValide =
+
+    $scope.isPass2Equal = function (invalide, dirty) {
+        $scope.isPass2Valide = !(!invalide && dirty && ($scope.newUser.userPass1 === $scope.newUser.userPass2));
+    };
+
+
+    $scope.submitUserForm = function (valid) {
+        if (valid) {
+            $scope.message = "We sent link to your e-mail. Please follow the link to activate your account";
+        }
+    };
+
+
+}
--- lesson_14/Homework/task1/module.js
+++
@@ -1,1 +0,0 @@
-var calculatorModule = angular.module('calculatorModule',[]);
