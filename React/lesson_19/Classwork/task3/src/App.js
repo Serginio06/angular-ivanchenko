46939fc4f3ade4b540e15e12693f0430473c3596
Sergii Ivanchenko <ivanchenko.s@gmail.com>
2017-07-03 11:24:43 +0300

    React. Lesson19. Classwork. Task 3

---
+++ React/lesson_19/Classwork/task3/src/App.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import './style/App.css';
+
+class App extends Component {
+
+
+    render() {
+        let sum = +this.props.summand1 + +this.props.summand2;
+
+        return (
+            <div className="container">
+                <div className="panel panel-primary">
+                    <div className="panel-heading panel-primary">Lesson 19. Sum of two numbers</div>
+                    <div className="panel-body">
+
+                        <h3 className="">The sum of {this.props.summand1} and {this.props.summand2} is equal
+                            to {sum}</h3>
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
