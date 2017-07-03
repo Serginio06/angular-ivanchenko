37680351a704361896894cbf74842e40df40ea39
Sergii Ivanchenko <ivanchenko.s@gmail.com>
2017-07-03 10:41:11 +0300

    React.Lesson19. Classwork. Task2

---
+++ React/lesson_19/Classwork/task2/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './style/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="well well-lg">
+        <h1>Hello world</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
